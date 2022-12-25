bcac836c2cc869f00c7c79e4c0af4e7bfa738b99
Mingon_Choe <skandy57@naver.com>
2022-12-25 14:30:38 +0900

    dd

---
+++ baekjun/src/baekjun/AplusB.java
@@ -0,0 +1,16 @@
+package baekjun;
+
+import java.util.Scanner;
+
+public class AplusB {
+	public static void main(String[] args) {
+		Scanner s1 = new Scanner(System.in);
+
+		int A = s1.nextInt();
+		int B = s1.nextInt();
+
+		System.out.println(A + B);
+
+	}
+}
+
