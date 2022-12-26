680bd3cf57dc63c4839925e14dd1cf4dd73c4a18
Mingon_Choe <skandy57@naver.com>
2022-12-26 11:42:28 +0900

    22.12.26

--- baekjun/src/level4_1차원배열/Main.java
+++ baekjun/src/level4_1차원배열/Main.java
@@ -2,30 +2,43 @@ package level4_1차원배열;
 
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		List<Integer> list = new ArrayList<>();
-		List<Double> list2 = new ArrayList<>();
-		int count = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < count; i++) {
-			list.add(Integer.parseInt(st.nextToken()));
-		}
-		double M = 0;
-		for (double i : list) {
-			if (i > M) {
-				M = i;
-			}
-		}
-		for (double i : list) {
-			list2.add(((i / M) * 100));
-		}
+        // 입력받을 점수의 집합의 개수
+        int n = sc.nextInt();
 
-		double sum = list2.stream().mapToDouble(Double::doubleValue).sum();
-		System.out.println(sum / count);
+        for (int i = 0; i < n; i++) {
+            // 점수 집합의 크기
+            int k = sc.nextInt();
+            // 점수 집합
+            int[] arr = new int[k];
+            // 점수 집합의 합
+            int sum = 0;
 
-	}
+            for (int j = 0; j < k; j++) {
+                arr[j] = sc.nextInt();
+                sum += arr[j];
+            }
+            // 점수 집합의 평균
+            double avg = (double) sum / k;
+
+            // 평균 이상인 점수의 개수
+            int count = 0;
+            for (int j = 0; j < k; j++) {
+                if (arr[j] > avg) {
+                    count++;
+                }
+            }
+            // 평균 이상인 점수의 비율
+            double result = (double) count / k * 100;
+            System.out.printf("%.3f%%\n", result);
+        }
+    }
 }
